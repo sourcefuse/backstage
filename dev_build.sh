381b4637d1fa1e293bdaac7d0c13b0eda69c13e6
jamescrowley321 <jamescrowley151@gmail.com>
2022-02-11 14:42:19 -0600

    minor changes related to k8s deployment

---
+++ dev_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION=$1
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 757583164619.dkr.ecr.us-east-1.amazonaws.com
+docker-compose build
+docker tag sourcefuse/sourcefuse-backstage:latest 757583164619.dkr.ecr.us-east-1.amazonaws.com/sf-refarch-dev-sourcefuse-backstage:${VERSION}
+docker push 757583164619.dkr.ecr.us-east-1.amazonaws.com/sf-refarch-dev-sourcefuse-backstage:${VERSION}
