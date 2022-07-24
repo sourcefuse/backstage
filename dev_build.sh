f2959aa5a4fb63be11649bd4c024c028483dbf9e
chrisdeffer <christopher.munson@sourcefuse.com>
2022-07-24 10:39:05 -0400

    feature/ARC-135-ci-config: push this PR

--- dev_build.sh
+++ dev_build.sh
@@ -5,3 +5,5 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker-compose build
 docker tag sourcefuse/sourcefuse-backstage:latest 757583164619.dkr.ecr.us-east-1.amazonaws.com/sf-refarch-dev-sourcefuse-backstage:${VERSION}
 docker push 757583164619.dkr.ecr.us-east-1.amazonaws.com/sf-refarch-dev-sourcefuse-backstage:${VERSION}
+
+
