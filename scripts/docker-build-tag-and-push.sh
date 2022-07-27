5114a0c2d24aa8fd7dbe6bff51bc4e8962061ec1
Travis Saucier <travis.saucier@sourcefuse.com>
2022-07-26 21:29:34 -0400

    add more output

--- scripts/docker-build-tag-and-push.sh
+++ scripts/docker-build-tag-and-push.sh
@@ -16,18 +16,20 @@ IMAGE_NAME="$ECR_REGISTRY_ENDPOINT/sf-refarch-dev-sourcefuse-backstage"
 
 echo "Account: $AWS_ACCOUNT_ID"
 
+printf "\nECR Login...\n"
 aws ecr get-login-password \
   --region $AWS_REGION \
   | docker login \
       --username AWS \
       --password-stdin $ECR_REGISTRY_ENDPOINT
 
+printf "\nBuilding docker images...\n"
 docker-compose -f $DOCKER_COMPOSE_FILE build
 
-printf "Tagging image...\n"
+printf "\nTagging image $IMAGE_NAME:$IMAGE_TAG...\n"
 docker tag sourcefuse/sourcefuse-backstage:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
 
-printf "Pushing image to ECR...\n"
+printf "\nPushing $IMAGE_NAME:$IMAGE_TAG to ECR...\n"
 docker push $IMAGE_NAME:$IMAGE_TAG
 
 echo "::set-output name=image::$IMAGE_NAME:$IMAGE_TAG"
