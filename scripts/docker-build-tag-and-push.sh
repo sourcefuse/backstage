e656c230d1aee5380668dab402c16c3121015822
Travis Saucier <travis.saucier@sourcefuse.com>
2022-07-26 21:06:43 -0400

    changing action to use script. update script to accept environment variable overrides.

---
+++ scripts/docker-build-tag-and-push.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## required
+: "${AWS_ACCOUNT_ID:=$AWS_ACCOUNT_ID}"
+#: "${AWS_ACCESS_KEY_ID:-$AWS_ACCESS_KEY_ID}"  # TODO - remove if not needed
+#: "${AWS_SECRET_ACCESS_KEY:-$AWS_ACCESS_KEY_ID}"  # TODO - remove if not needed
+: "${ECR_REGISTRY_ENDPOINT:-$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com}"
+: "${IMAGE_NAME:-$ECR_REGISTRY_ENDPOINT/sf-refarch-dev-sourcefuse-backstage}"
+: "${IMAGE_TAG:-$IMAGE_TAG}"
+
+# optional
+: "${AWS_REGION:=us-east-1}"
+: "${DOCKER_COMPOSE_FILE:=./docker-compose.yml}"
+
+echo "Account: $AWS_ACCOUNT_ID"
+
+aws ecr get-login-password \
+  --region $AWS_REGION \
+  | docker login \
+      --username AWS \
+      --password-stdin $ECR_REGISTRY_ENDPOINT
+
+docker-compose -f $DOCKER_COMPOSE_FILE build
+
+printf "Tagging image...\n"
+docker tag sourcefuse/sourcefuse-backstage:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
+
+printf "Pushing image to ECR...\n"
+docker push $IMAGE_NAME:$IMAGE_TAG
+
+echo "::set-output name=image::$IMAGE_NAME:$IMAGE_TAG"
+
+
