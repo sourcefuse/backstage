f335f4f30061f84a1f6f5cd99a9890b8979def15
Travis Saucier <travis.saucier@sourcefuse.com>
2022-07-26 21:22:58 -0400

    fix variables.

--- scripts/docker-build-tag-and-push.sh
+++ scripts/docker-build-tag-and-push.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
+source $PWD/.secrets.sh
+
 # optional
 : "${AWS_REGION:=us-east-1}"
 : "${DOCKER_COMPOSE_FILE:=./docker-compose.yml}"
 
 ## required
-: "${AWS_ACCOUNT_ID:=$AWS_ACCOUNT_ID}"
+: "${AWS_ACCOUNT_ID:-$AWS_ACCOUNT_ID}"
+: "${IMAGE_TAG:-$IMAGE_TAG}"
 #: "${AWS_ACCESS_KEY_ID:-$AWS_ACCESS_KEY_ID}"  # TODO - remove if not needed
 #: "${AWS_SECRET_ACCESS_KEY:-$AWS_ACCESS_KEY_ID}"  # TODO - remove if not needed
-: "${ECR_REGISTRY_ENDPOINT:-$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com}"
-: "${IMAGE_NAME:-$ECR_REGISTRY_ENDPOINT/sf-refarch-dev-sourcefuse-backstage}"
-: "${IMAGE_TAG:-$IMAGE_TAG}"
+ECR_REGISTRY_ENDPOINT="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+IMAGE_NAME="$ECR_REGISTRY_ENDPOINT/sf-refarch-dev-sourcefuse-backstage"
 
 echo "Account: $AWS_ACCOUNT_ID"
 
-echo $IMAGE_NAME:$IMAGE_TAG
+aws ecr get-login-password \
+  --region $AWS_REGION \
+  | docker login \
+      --username AWS \
+      --password-stdin $ECR_REGISTRY_ENDPOINT
 
-#aws ecr get-login-password \
-#  --region $AWS_REGION \
-#  | docker login \
-#      --username AWS \
-#      --password-stdin $ECR_REGISTRY_ENDPOINT
-#
-#docker-compose -f $DOCKER_COMPOSE_FILE build
-#
-#printf "Tagging image...\n"
-#docker tag sourcefuse/sourcefuse-backstage:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
-#
-#printf "Pushing image to ECR...\n"
-#docker push $IMAGE_NAME:$IMAGE_TAG
-#
-#
-#
-#echo "::set-output name=image::$IMAGE_NAME:$IMAGE_TAG"
-#
-#
+docker-compose -f $DOCKER_COMPOSE_FILE build
+
+printf "Tagging image...\n"
+docker tag sourcefuse/sourcefuse-backstage:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
+
+printf "Pushing image to ECR...\n"
+docker push $IMAGE_NAME:$IMAGE_TAG
+
+echo "::set-output name=image::$IMAGE_NAME:$IMAGE_TAG"
