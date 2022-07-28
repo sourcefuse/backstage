a699fd17de41844e2cce3aa36b58fff65c67658e
Travis Saucier <travis.saucier@sourcefuse.com>
2022-07-27 22:22:47 -0400

    updating tag from docker-compose build

--- scripts/docker-build-tag-and-push.sh
+++ scripts/docker-build-tag-and-push.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # optional
 : "${AWS_REGION:=us-east-1}"
 : "${DOCKER_COMPOSE_FILE:=./docker-compose.yml}"
+: "${ENVIRONMENT:=dev}"
 
 ## required
 : "${AWS_ACCOUNT_ID:-$AWS_ACCOUNT_ID}"
 : "${IMAGE_TAG:-$IMAGE_TAG}"
 
 ECR_REGISTRY_ENDPOINT="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
-IMAGE_NAME="$ECR_REGISTRY_ENDPOINT/sf-refarch-dev-sourcefuse-backstage"
+IMAGE_NAME="$ECR_REGISTRY_ENDPOINT/sf-refarch-$ENVIRONMENT-sourcefuse-backstage"
 
 echo "Account: $AWS_ACCOUNT_ID"
 
@@ -24,7 +27,7 @@ printf "\nBuilding docker images...\n"
 docker-compose -f $DOCKER_COMPOSE_FILE build
 
 printf "\nTagging image $IMAGE_NAME:$IMAGE_TAG...\n"
-docker tag sourcefuse/sourcefuse-backstage:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
+docker tag sourcefuse/sourcefuse-backstage:latest $IMAGE_NAME:$IMAGE_TAG
 
 printf "\nPushing $IMAGE_NAME:$IMAGE_TAG to ECR...\n"
 docker push $IMAGE_NAME:$IMAGE_TAG
