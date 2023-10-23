d260d4c5a649559ba204efc7316fe55f1e75af53
Hitesh Gupta <hiteshgupta3012@gmail.com>
2023-10-23 15:49:00 +0530

    fix: Setup for Multi org provider for github
    Signed-off-by: Hitesh Gupta <hiteshgupta3012@gmail.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,8 +1,7 @@
 import {CatalogBuilder} from '@backstage/plugin-catalog-backend';
 import {ScaffolderEntitiesProcessor} from '@backstage/plugin-scaffolder-backend';
 // import {GithubOrgEntityProvider, GithubOrgReaderProcessor} from "@backstage/plugin-catalog-backend-module-github";
-import {GithubOrgReaderProcessor} from "@backstage/plugin-catalog-backend-module-github";
-// import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import {GithubMultiOrgEntityProvider, GithubOrgReaderProcessor} from "@backstage/plugin-catalog-backend-module-github";
 
 import {Router} from 'express';
 import {PluginEnvironment} from '../types';
@@ -15,18 +14,20 @@ export default async function createPlugin(
     builder.addProcessor(
         GithubOrgReaderProcessor.fromConfig(env.config, {logger: env.logger}),
     );
+    const githubIngtigrationConfig: any = env.config.get('integrations.github');
     // The org URL below needs to match a configured integrations.github entry
     // specified in your app-config.
-    // const gitProvider = GithubOrgEntityProvider.fromConfig(env.config, {
-    //     id: 'production',
-    //     orgUrl: 'https://github.com/sourcefuse', // TODO: inject from config
-    //     logger: env.logger,
-    //     schedule: env.scheduler.createScheduledTaskRunner({
-    //         frequency: {minutes: 120}, // TODO: inject from config
-    //         timeout: {minutes: 15}, // TODO: inject from config
-    //     }),
-    // });
-    // builder.addEntityProvider(gitProvider as GithubOrgEntityProvider);
+    const gitProvider = GithubMultiOrgEntityProvider.fromConfig(env.config, {
+        id: 'production',
+        githubUrl: 'https://github.com',
+        orgs: githubIngtigrationConfig[0].orgs,
+        logger: env.logger,
+        // schedule: env.scheduler.createScheduledTaskRunner({
+        //     frequency: {minutes: 120}, // TODO: inject from config
+        //     timeout: {minutes: 15}, // TODO: inject from config
+        // }),
+    });
+    builder.addEntityProvider(gitProvider as GithubMultiOrgEntityProvider);
     // builder.addEntityProvider(
     //     GithubEntityProvider.fromConfig(env.config, {
     //         logger: env.logger,
@@ -42,6 +43,8 @@ export default async function createPlugin(
 
     const {processingEngine, router} = await builder.build();
     await processingEngine.start();
-    // await gitProvider.read();
+    if (process.env.ENABLE_GITHUB_SYNC) {
+        await gitProvider.read();
+    }
     return router;
 }
