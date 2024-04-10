83f8d8bcdb3821bea29d5b944d3710306b13924e
sadarunnisa-sf <sadarunnisa.sutar@sourcefuse.com>
2024-04-10 13:14:39 +0530

    feat(chore): push images to dockerhub
    push images to dockerhub
    
    ARC-108

---
+++ scripts/docker-build-tag-and-push-dockerhub.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+# Optional
+: "${DOCKER_COMPOSE_FILE:=./docker-compose.yml}"
+: "${ENVIRONMENT:=poc}"
+
+# Required
+: "${DOCKER_USERNAME:-$DOCKER_USERNAME}"
+: "${DOCKER_PASSWORD:-$DOCKER_PASSWORD}"
+: "${IMAGE_TAG:-$IMAGE_TAG}"
+
+DOCKER_REGISTRY="docker.io"
+IMAGE_NAME="$DOCKER_USERNAME/backstage"
+
+echo "Docker Registry: $DOCKER_REGISTRY"
+
+printf "\nLogging in to Docker Hub...\n"
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+printf "\nBuilding docker images...\n"
+docker-compose -f $DOCKER_COMPOSE_FILE build
+
+printf "\nTagging image $IMAGE_NAME:$IMAGE_TAG...\n"
+docker tag sourcefuse/backstage:latest $IMAGE_NAME:$IMAGE_TAG
+
+printf "\nPushing $IMAGE_NAME:$IMAGE_TAG to Docker Hub...\n"
+docker push $IMAGE_NAME:$IMAGE_TAG
+
+# printf "\nAdding $IMAGE_NAME:$IMAGE_TAG to SSM Parameter...\n"
+# aws ssm put-parameter \
+#   --name "/${ENVIRONMENT}/backstage/container-image" \
+#   --description "Container image reference for downstream deployments" \
+#   --value "$IMAGE_NAME:$IMAGE_TAG" \
+#   --type String \
+#   --overwrite
+
+# echo "::set-output name=image::$IMAGE_NAME:$IMAGE_TAG"
