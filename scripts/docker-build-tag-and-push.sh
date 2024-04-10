5c9ab7db20f34f2da82b6b504e5fb44b8e6a3312
sadarunnisa-sf <sadarunnisa.sutar@sourcefuse.com>
2024-04-10 15:32:56 +0530

    feat(chore): push images to dockerhub
    push images to dockerhub
    
    ARC-108

--- scripts/docker-build-tag-and-push.sh
+++ scripts/docker-build-tag-and-push.sh
@@ -17,8 +17,8 @@ IMAGE_NAME="$DOCKER_USERNAME/backstage"
 echo "Docker Registry: $DOCKER_REGISTRY"
 
 printf "\nLogging in to Docker Hub...\n"
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-# docker login --username "$DOCKER_USERNAME" --password-stdin "$DOCKER_PASSWORD"
+# echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
 
 printf "\nBuilding docker images...\n"
