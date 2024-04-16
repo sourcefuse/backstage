2b17450dcf2056fb82c5d44236f8c53b246c872a
sadarunnisa <sadarunnisa.sutar@sourcefuse.com>
2024-04-16 11:41:43 +0530

    feat(chore): push images to dockerhub (#133)
    Move docker Image to Docker Hub and Update Docker Image Everywhere
    
    ARC-108
    
    ## Description
    - Move docker Image to Docker Hub
    - Update Docker Image Everywhere (Move from
    sourcefuse/sourcefuse-backstage to sourcefuse/backstage.)
    - Created dockerhub public repository as 'backstage'
    - Added Repository secrets for github username and password
    (SF_ARCH_DOCKERHUB_USERNAME, SF_ARCH_DOCKERHUB_PASSWORD)
    - Made required changes in github workflow files and docker-compose.yml
    
    Fixes # (issue)
    https://github.com/sourcefuse/backstage/issues/108 (Move docker Image to
    Docker Hub)
    https://github.com/sourcefuse/backstage/issues/109 (Update Docker Image
    Everywhere)
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Intermediate change (work in progress)
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    
    - [ ] Test A
    - [ ] Test B
    
    ## Checklist:
    
    - [X] Performed a self-review of my own code
    - [X] npm test passes on your machine
    - [ ] New tests added or existing tests modified to cover all changes
    - [ ] Code conforms with the style guide
    - [ ] API Documentation in code was updated
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- scripts/docker-build-tag-and-push.sh
+++ scripts/docker-build-tag-and-push.sh
@@ -2,34 +2,32 @@
 
 set -e
 
-# optional
-: "${AWS_REGION:=us-east-1}"
+# Optional
 : "${DOCKER_COMPOSE_FILE:=./docker-compose.yml}"
 : "${ENVIRONMENT:=poc}"
 
-## required
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+# Required
+: "${DOCKER_USERNAME:=$DOCKERHUB_USERNAME}"
+: "${DOCKER_PASSWORD:=$DOCKERHUB_PASSWORD}"
 : "${IMAGE_TAG:-$IMAGE_TAG}"
 
-ECR_REGISTRY_ENDPOINT="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
-IMAGE_NAME="$ECR_REGISTRY_ENDPOINT/sourcefuse-backstage"
+DOCKERHUB_REGISTRY="docker.io"
+IMAGE_NAME="$DOCKERHUB_REGISTRY/$DOCKER_USERNAME/backstage"
 
-echo "Account: $AWS_ACCOUNT_ID"
+echo "Docker Registry: $DOCKERHUB_REGISTRY ...\n"
 
-printf "\nECR Login...\n"
-aws ecr get-login-password \
-  --region $AWS_REGION \
-  | docker login \
-      --username AWS \
-      --password-stdin $ECR_REGISTRY_ENDPOINT
+printf "\nLogging in to Docker Hub... -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ImangeName $IMAGE_NAME Imagetag $IMAGE_TAG ...\n"
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
 
 printf "\nBuilding docker images...\n"
 docker-compose -f $DOCKER_COMPOSE_FILE build
 
 printf "\nTagging image $IMAGE_NAME:$IMAGE_TAG...\n"
-docker tag sourcefuse/sourcefuse-backstage:latest $IMAGE_NAME:$IMAGE_TAG
+docker images
+docker tag sourcefuse/backstage:latest $IMAGE_NAME:$IMAGE_TAG
 
-printf "\nPushing $IMAGE_NAME:$IMAGE_TAG to ECR...\n"
+printf "\nPushing $IMAGE_NAME:$IMAGE_TAG to Docker Hub...\n"
 docker push $IMAGE_NAME:$IMAGE_TAG
 
 printf "\nAdding $IMAGE_NAME:$IMAGE_TAG to SSM Parameter...\n"
