a697b1d8f4b327bd49e7b005c28ebfc8891f8c01
sadarunnisa-sf <sadarunnisa.sutar@sourcefuse.com>
2024-06-06 04:02:03 +0530

    feat(plugins): validate user access for scaffold
    validate user access for scaffold
    
    ARC-149

--- packages/app/src/PermissionWrapper.tsx
+++ packages/app/src/PermissionWrapper.tsx
@@ -1,14 +1,10 @@
 // PermissionWrapper.js
 
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { usePermissionCheck } from './usePermissionCheck';
-// Import the custom hook
-interface IPermissionWrapper {
-  children?: ReactNode;
-  permission?: string;
-}
-export const PermissionWrapper = ({ children, permission }: IPermissionWrapper) => { // NOSONAR
-  const hasPermission = usePermissionCheck(permission as string);
+
+export const PermissionWrapper = ({ children, permission }: any) => { // NOSONAR
+  const hasPermission = usePermissionCheck(permission as string); // NOSONAR
 
   if (hasPermission === null) {
     return <div>Loading...</div>; // Render a loading state while checking permissions
