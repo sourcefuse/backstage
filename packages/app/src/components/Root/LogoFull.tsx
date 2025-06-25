8b9d3b8552eb7bf45e685f675ef5eed7f3db90ad
vijay-stephen <91307015+vijay-stephen@users.noreply.github.com>
2025-06-25 22:11:08 +0530

    Update LogoFull.tsx

--- packages/app/src/components/Root/LogoFull.tsx
+++ packages/app/src/components/Root/LogoFull.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 import arcLogo from './logo/arc.png';
 
-const useStyles = makeStyles({
-  backstagelogoHolder: {
-    width: '100%',
-    padding: '10px',
-    background: '#black',
-    borderRadius: '4px',
-  },
-  svgback: {
-    width: 'auto',
-    height: 30,
-  },
-  pathback: {
-    fill: '#E81823',
-  },
-});
 const LogoFull = () => {
   return <img src={arcLogo} />;
 };
